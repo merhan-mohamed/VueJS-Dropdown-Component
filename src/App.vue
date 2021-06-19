a75ae60b474ac4e97e6be1ae99bd61b8ba9423fd
merhan-mohamed <39986371+merhan-mohamed@users.noreply.github.com>
2021-06-19 18:02:20 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <header>
+      <Navbar />
+    </header>
+  </div>
+</template>
+
+<script type="text/javascript">
+import Navbar from "./components/Navbar";
+export default {
+  name: "app",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "montserrat", sans-serif;
+}
+header {
+  width: 100%;
+  background-color: #222;
+  padding: 15px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template lang="html">
+  <nav>
+    <div class="menu-items"><a href="#">Home</a></div>
+    <div class="menu-items"><a href="#">About</a></div>
+    <dropdown title='services' :items='services'/>
+    <div class="menu-items"><a href="#">Contact</a></div>
+  </nav>
+</template>
+
+<script>
+import dropdown from "./dropdown.vue";
+export default {
+  name: "navbar",
+  components: {
+    dropdown,
+  },
+  data: () => ({
+    services: [
+      {
+        title: "Web",
+        link: "#",
+      },
+      {
+        title: "Design",
+        link: "#",
+      },
+      {
+        title: "Videos",
+        link: "#",
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+nav .menu-items.active,
+nav .menu-items:hover {
+  background-color: #444;
+  border-bottom-color: #ff5858;
+}
+nav .menu-items {
+  padding: 10px 20px;
+  color: white;
+  text-align: center;
+  border-bottom: 3px solid transparent;
+  position: relative;
+}
+
+nav .menu-items a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
